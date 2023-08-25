445bf498a0dbaad27fbed25147401f4874940279
Ian Miller <indmillr@gmail.com>
2023-08-25 11:48:44 -0500

    Home Page

--- components/Layout.js
+++ components/Layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
     <div
       className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
     >
-      <TopLeftImg />
+      {/*<TopLeftImg />*/}
       <Nav />
       <Header />
       {children}
