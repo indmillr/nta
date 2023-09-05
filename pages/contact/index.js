ec085249703f29ae5505705ba15b1c6b14d5818c
indmillr <indmillr@gmail.com>
2023-09-05 13:40:49 -0500

    EmailJs

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,5 +1,5 @@
-// components
-import Circles from "/components/Circles";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 // icons
 import { BsArrowRight } from "react-icons/bs";
@@ -11,6 +11,21 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_kbavyqc', 'template_oib19lw', form.current, 'iC7Fva3covAMd6Kef')
+      .then((result) => {
+        console.log(result.text);
+        e.target.reset();
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <div className="h-full bg-primary text-black">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
@@ -51,14 +66,16 @@ const Contact = () => {
             animate="show"
             exit="hidden"
             className="flex-1 flex flex-col gap-6 w-full mx-auto"
+            ref={form}
+            onSubmit={sendEmail}
           >
             {/* input group */}
             <div className="flex gap-x-6 w-full">
-              <input type="text" placeholder="name" className="input" />
-              <input type="text" placeholder="email" className="input" />
+              <input type="text" placeholder="name" className="input" name="user_name" />
+              <input type="text" placeholder="email" className="input" name="user_email" />
             </div>
-            <textarea placeholder="message" className="textarea"></textarea>
-            <button className="btn rounded-full border border-black max-w-[170px] px-8 transition-all duration-300 hover:bg-blue-600 flex items-center justify-center overflow-hidden hover:border-accent group">
+            <textarea placeholder="message" className="textarea" name="user_message"></textarea>
+            <button type="submit" className="btn rounded-full border border-black max-w-[170px] px-8 transition-all duration-300 hover:bg-blue-600 flex items-center justify-center overflow-hidden hover:border-accent group">
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
                 Send
               </span>
