5a9439ace7a55853ad95745d0d8b512ad4336949
indmillr <indmillr@gmail.com>
2023-09-05 09:37:32 -0500

    shrink navbar

--- components/Nav.js
+++ components/Nav.js
@@ -43,7 +43,7 @@ const Nav = () => {
   return (
     <nav className="flex flex-col items-center xl:justify-center fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen gap-y-4">
       {/* inner */}
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-black/80 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[40px] xl:h-max py-8 bg-black/80 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((link, index) => {
           return (
             <Link
