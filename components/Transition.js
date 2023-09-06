7e8ec236f32c1e108377edba4b04d1283bd7555f
indmillr <indmillr@gmail.com>
2023-09-06 16:04:19 -0500

    turn off Transition

--- components/Transition.js
+++ components/Transition.js
@@ -27,8 +27,7 @@ const Transition = () => {
         animate="animate"
         exit="exit"
         transition={{ delay: 0, duration: 0.1, ease: "easeInOut" }}
-      >
-      </motion.div>
+      ></motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#f13024]"
         variants={transitionVariants}
@@ -36,8 +35,7 @@ const Transition = () => {
         animate="animate"
         exit="exit"
         transition={{ delay: 0.1, duration: 0.2, ease: "easeInOut" }}
-      >
-      </motion.div>
+      ></motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#111111]"
         variants={transitionVariants}
@@ -45,8 +43,7 @@ const Transition = () => {
         animate="animate"
         exit="exit"
         transition={{ delay: 0.2, duration: 0.3, ease: "easeInOut" }}
-      >
-      </motion.div>
+      ></motion.div>
     </>
   );
 };
